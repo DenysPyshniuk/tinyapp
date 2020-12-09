9cd06f80fc40db2db47e2e7def9f472c732f8fea
DenysPyshniuk <denys.pyshniuk@gmail.com>
2020-12-09 00:19:57 +0000

    Now user can do login and will see the information that he is LoggedIn

--- express_server.js
+++ express_server.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 8080; // default port 8080
 
-const bodyParser = require("body-parser");
+app.set("view engine", "ejs");
+
+app.use(morgan("dev"));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs");
+const urlDatabase = {
+  'b2xVn2': "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
+
+
 
 /** Function to generate new random 6 digit string short URL */
 function generateRandomString() {
@@ -17,46 +29,69 @@ function generateRandomString() {
   return randomURL;
 }
 
-
-const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-};
+//////           APP.GET        ////////
 
 /** Home Page rout */
 app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
+/** Login rout */
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 /** URL page rout */
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 /** Submit NEW URL page rout */
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 /** Update URL page rout */
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
-  const templateVars = { shortURL, longURL };
+  const templateVars = {
+    shortURL,
+    longURL,
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
-
-
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  if (!urlDatabase[req.params.shortURL]) {
+    res.statusCode = 400;
+    return res.send(
+      "The page you have requested does not exist. Please check to make sure you've entered the correct Tiny URL and try again :)"
+    );
+  }
+  res.redirect(longURL);
+});
+
+
+//////           APP.POST         ////////
 
 app.post("/urls", (req, res) => {
   res.statusCode = 200;
@@ -64,18 +99,6 @@ app.post("/urls", (req, res) => {
   console.log('New Short URL : ', newShortURL);
   urlDatabase[newShortURL] = req.body.longURL;
   res.redirect(`/urls/${newShortURL}`);
-});
-
-
-app.get("/u/:shortURL", (req, res) => {
-
-  const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
-  if(!urlDatabase[req.params.shortURL]) {
-    res.statusCode = 400;
-    return res.send("The page you have requested does not exist. Please check to make sure you've entered the correct Tiny URL and try again :)");
-  }
-  res.redirect(longURL);
 });
 
 /** Deletes the short URL from database */
@@ -95,12 +118,17 @@ app.post("/urls/:id", (req, res) => {
 
 /** Login Post rout */
 app.post("/login", (req, res) => {
-  let name = request.body.username;
-  res.cookie('username');
-
-  // res.redirect(`/urls`);
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect(`/urls`);
 });
 
+// /** Logout Post rout */
+// app.post("/logout", (req, res) => {
+//   const username = req.body.username;
+//   res.clearCookie('username', username);
+//   res.redirect(`/urls`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
